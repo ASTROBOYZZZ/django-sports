1b1fdf7cddcb25cba00cfc89ccf905830374b89f
feixiong <1459576020@qq.com>
2023-03-16 18:27:09 +0800

    =models更新

--- sportsequipment/app/models.py
+++ sportsequipment/app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    uid = models.AutoField('用户id', primary_key=True)
+    uid = models.CharField('学号', max_length=20, null=False, primary_key=True)
     name = models.CharField("姓名", max_length=20, null=False)
-    password = models.CharField("密码", max_length=20, null=False)
-
+    password = models.CharField("密码", max_length=12, null=False)
+    email = models.CharField("邮箱",max_length=20, null=False,default="1459576020@qq.com")
     class Meta:
         db_table = "users"
 
